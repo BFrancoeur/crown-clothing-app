733ce9a91c5ec6e122b9908768845acc532ed310
Brian Francoeur <francoeurbj@gmail.com>
2022-05-04 14:32:56 -0400

    create button component

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,26 +1,22 @@
-/*
- Button Types:
-
- Default (black w/white text)
-
- Inverted
-
- Google Sign-in
-*/
 import './button.styles.scss';
 
 const BUTTON_TYPE_CLASSES = {
-    google: 'google-sign-in',
-    inverted: 'inverted',
+    "google": 'google-sign-in',
+    "inverted": 'inverted',
 }
 
 const Button = ({ children, buttonType, ...otherProps}) => {
-    <button 
-        className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`} 
-        {...otherProps}
-    >
-        Sign Up Button
+
+    return (
+
+        <button 
+            className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`} 
+            {...otherProps}
+        >
+        {children}
     </button>
+
+    );
 }
 
 export default Button;
