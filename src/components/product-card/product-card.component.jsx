2b7836a5d5d01b3d3ac3c65fe2e24513b01ebca4
Brian Francoeur <francoeurbj@gmail.com>
2022-05-09 15:12:22 -0400

    added addToCart feature

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,3 +1,7 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
+
 import Button from '../button/button.component';
 
 import './product-card.styles.scss';
@@ -5,6 +9,9 @@ import './product-card.styles.scss';
 const ProductCard = ({ product }) => {
 
     const { name, price, imageUrl } = product;
+    const { addItemToCart } = useContext(CartContext);
+
+    const addProductToCart = () => addItemToCart(product);
 
     return (
 
@@ -17,7 +24,9 @@ const ProductCard = ({ product }) => {
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
             </div>
-            <Button buttonType="inverted">Add To Cart</Button>
+            <Button buttonType="inverted" onClick={addProductToCart}>
+                Add To Cart
+            </Button>
         </div>
     );
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.scss";
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
 import { ProductsProvider } from "./contexts/products.context";
 import { CartProvider } from "./contexts/cart.context";
+
+import "./index.scss";
 
 // import reportWebVitals from "./reportWebVitals";
 
